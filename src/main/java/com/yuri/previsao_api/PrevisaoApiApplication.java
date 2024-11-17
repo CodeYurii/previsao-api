11db1b16a2ed444c2c55b711ae7342e59a3e7024
CodeYurii <yuri.seixeiro@gmail.com>
2024-11-16 22:29:22 -0300

    primeiro commit

---
+++ src/main/java/com/yuri/previsao_api/PrevisaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.yuri.previsao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevisaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrevisaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuri/previsao_api/PrevisaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuri.previsao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrevisaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
